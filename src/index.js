18343eaf4ccf1180930e49c369d9e1572d901aae
stevelimly <49090381+stevelimly@users.noreply.github.com>
2022-11-05 23:04:30 +0800

    Add api.js file in src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'
+import App from './Api'
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 
